0542cf22198a572bbf6286b3ec5ff2eaee38d4ae
Bill <bassguitarbill@gmail.com>
2015-10-02 01:14:04 -0400

    Add items and customizable names

--- zelda.js
+++ zelda.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$(".triforce").click(function(ev){
 		if(ev.toElement.src.includes("triforce-hover.png")){
 			ev.toElement.src = "images/triforce.png";
@@ -16,4 +17,17 @@ $(document).ready(function(){
 		} 	
 	});
 	console.log("done");
+	
+	$(".name").click(function(ev){
+		var runnerName = prompt("Enter the name");
+		ev.target.innerText = runnerName || "LUNK";
+	});
+	
+	$(".magic-sword,.candle,.bow,.raft,.ladder,.flute,.arrow").click(function(ev){
+		if(ev.toElement.src.includes("-off.png")){
+			ev.toElement.src = ev.toElement.src.replace("-off.png",".png");
+		} else {
+			ev.toElement.src = ev.toElement.src.replace(".png","-off.png");
+		}
+	});
 });
